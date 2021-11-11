4cdb05dd517bc35802b6ed0f7f6d38e1a7032c8f
David Ryan <dryan19@unl.edu@MacBook-Air.local>
2021-11-10 20:40:34 -0600

    changed from json to csv data format

--- monte/main.cpp
+++ monte/main.cpp
@@ -1,78 +1,178 @@
 #include <fstream>
+#include <istream>
 #include <iostream>
 #include <vector>
+#include <string>
 
 #include <chrono>
 #include <random>
 
-#include "json.hpp"
 
-using json = nlohmann::json;
+
+
+
+enum class CSVState {
+    UnquotedField,
+    QuotedField,
+    QuotedQuote
+};
+
+std::vector<std::string> readCSVRow(const std::string &row) {
+    CSVState state = CSVState::UnquotedField;
+    std::vector<std::string> fields {""};
+    size_t i = 0; // index of the current field
+    for (char c : row) {
+        switch (state) {
+            case CSVState::UnquotedField:
+                switch (c) {
+                    case ',': // end of field
+                              fields.push_back(""); i++;
+                              break;
+                    case '"': state = CSVState::QuotedField;
+                              break;
+                    default:  fields[i].push_back(c);
+                              break; }
+                break;
+            case CSVState::QuotedField:
+                switch (c) {
+                    case '"': state = CSVState::QuotedQuote;
+                              break;
+                    default:  fields[i].push_back(c);
+                              break; }
+                break;
+            case CSVState::QuotedQuote:
+                switch (c) {
+                    case ',': // , after closing quote
+                              fields.push_back(""); i++;
+                              state = CSVState::UnquotedField;
+                              break;
+                    case '"': // "" -> "
+                              fields[i].push_back('"');
+                              state = CSVState::QuotedField;
+                              break;
+                    default:  // end of quote
+                              state = CSVState::UnquotedField;
+                              break; }
+                break;
+        }
+    }
+    return fields;
+}
+
+/// Read CSV file, Excel dialect. Accept "quoted fields ""with quotes"""
+std::vector<std::vector<std::string>> readCSV(std::istream &in) {
+    std::vector<std::vector<std::string>> table;
+    std::string row;
+    while (!in.eof()) {
+        std::getline(in, row);
+        if (in.bad() || in.fail()) {
+            break;
+        }
+        auto fields = readCSVRow(row);
+        table.push_back(fields);
+    }
+    return table;
+}
 
 int main(int argc, char const *argv[])
 {
     /* code */
 
-    // std::ifstream i("data.json");
+
+    //std::ifstream f("data.csv");
+
+    std::istream
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+    // std::ifstream f("data.json");
     // json j;
-    // i >> j;
+    // f >> j;
 
-    // std::ofstream o("pretty.json");
-    // o << std::setw(4) << j << std::endl;
+    // j.
 
-    // std::vector<std::uint8_t> v = {'t', 'r', 'u', 'e'};
-    // j = json::parse(v.begin(), v.end());
+    // // std::ofstream o("pretty.json");
+    // // o << std::setw(4) << j << std::endl;
 
-    // for (json::iterator it = j.begin(); it != j.end(); ++it) {
-    //   std::cout << *it << '\n';
-    // }
+    // // std::vector<std::uint8_t> v = {'t', 'r', 'u', 'e'};
+    // // j = json::parse(v.begin(), v.end());
+
+    // // for (json::iterator it = j.begin(); it != j.end(); ++it) {
+    // //   std::cout << *it << '\n';
+    // // }
+
+    // j.begin();
+
+
 
     // for (json::iterator it = j.begin(); it != j.end(); ++it)
     // {
-    //     std::cout << it.key() << " : " << it.value() << "\n";
+    //     std::cout << /** it.key() << */" : " << it.value() << "\n";
+
+        
     // }
 
-    // std::cout << "File size: " << j.size() << std::endl;
-
-    std::vector<double> v;
 
 
-    std::random_device rand;
-    std::mt19937 generate(rand());
-    std::uniform_real_distribution<double> dist(0, 100);
+    // // std::cout << "File size: " << j.size() << std::endl;
 
-    int length = 10 * 100;
+    // std::vector<double> v;
 
-    int guess = 0;
 
-    double hit = 0;
-    double miss = 0;
+    // std::random_device rand;
+    // std::mt19937 generate(rand());
+    // std::uniform_real_distribution<double> dist(0, 100);
 
-    for (double i = 0; i < length; i = i + 0.1)
-    {
-        v.push_back(i);
-    }
+    // int length = 10 * 100;
+    // int guess = 0;
+    // double hit = 0;
+    // double miss = 0;
 
-    std::cout << v.at(1000) << std::endl;
+    // for (double i = 0; i < length; i = i + 0.1)
+    // {
+    //     v.push_back(i);
+    // }
 
-    for (int i = 0; i < length; i++)
-    {
+    // std::cout << v.at(1000) << std::endl;
 
-        guess = dist(generate);
+    // // for (int i = 0; i < length; i++)
+    // // {
 
-        if (guess < v.at(i))
-        {
-            hit++;
-        }
-        else
-        {
-            miss++;
-        }
-    }
+    // //     guess = dist(generate);
 
-    double ratio = hit / miss;
+    // //     if (guess < v.at(i))
+    // //     {
+    // //         hit++;
+    // //     }
+    // //     else
+    // //     {
+    // //         miss++;
+    // //     }
+    // // }
 
-    std::cout << "Ratio: " << ratio << " | Hits: " << hit << " | Misses: " << miss << std::endl;
+    // double ratio = hit / miss;
+
+    // std::cout << "Ratio: " << ratio << " | Hits: " << hit << " | Misses: " << miss << 
+    
+    // std::endl;
+
+    
 
     return 0;
 }
