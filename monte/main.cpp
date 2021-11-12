9b91620bba516807da7ea1b2a0bcc9d99ba37258
David Ryan <dryan19@unl.edu@MacBook-Air.local>
2021-11-11 19:00:30 -0600

    roughed out algo and got a value

--- monte/main.cpp
+++ monte/main.cpp
@@ -7,64 +7,26 @@
 #include <chrono>
 #include <random>
 
-enum class CSVState
+struct max_min
 {
-    UnquotedField,
-    QuotedField,
-    QuotedQuote
+    int max;
+    int min;
 };
 
 std::vector<std::string> readCSVRow(const std::string &row)
 {
-    CSVState state = CSVState::UnquotedField;
     std::vector<std::string> fields{""};
     size_t i = 0; // index of the current field
     for (char c : row)
     {
-        switch (state)
+        switch (c)
         {
-        case CSVState::UnquotedField:
-            switch (c)
-            {
-            case ',': // end of field
-                fields.push_back("");
-                i++;
-                break;
-            case '"':
-                state = CSVState::QuotedField;
-                break;
-            default:
-                fields[i].push_back(c);
-                break;
-            }
+        case ',': // end of field
+            fields.push_back("");
+            i++;
             break;
-        case CSVState::QuotedField:
-            switch (c)
-            {
-            case '"':
-                state = CSVState::QuotedQuote;
-                break;
-            default:
-                fields[i].push_back(c);
-                break;
-            }
-            break;
-        case CSVState::QuotedQuote:
-            switch (c)
-            {
-            case ',': // , after closing quote
-                fields.push_back("");
-                i++;
-                state = CSVState::UnquotedField;
-                break;
-            case '"': // "" -> "
-                fields[i].push_back('"');
-                state = CSVState::QuotedField;
-                break;
-            default: // end of quote
-                state = CSVState::UnquotedField;
-                break;
-            }
+        default:
+            fields[i].push_back(c);
             break;
         }
     }
@@ -105,74 +67,75 @@ int main(int argc, char const *argv[])
 
     data = readCSV(is);
 
-    std::cout << data.size() << std::endl;
+    // print test output
+    // std::cout << data.size() << std::endl;
+    // std::cout << data.at(0).size()
+    //           << std::endl;
+    // std::cout << data.at(2).at(1) << std::endl;
 
-    std::cout << data.at(0).size()
-              << std::endl;
+    // std::pair<int, int> maxMin = {0, 0};
 
-    std::pair<double, double> maxMin = {0,0};
+    struct max_min extrema = {0, 0};
 
+    // find max and min values
     for (int i = 0; i < data.size(); i++)
     {
-        if(data.at(i).at(1) > maxMin.first)
+        int dataPoint = std::stoi(data.at(i).at(1));
+        if (dataPoint > extrema.max)
+        {
+            extrema.max = dataPoint;
+        }
+        else if (dataPoint < extrema.min)
+        {
+            extrema.min = dataPoint;
+        };
     }
-    
 
-
-
-
-    // TODO: Find max and min values
+    // print max and min
+    std::cout << "max: " << extrema.max << std::endl;
+    std::cout << "min: " << extrema.min << std::endl;
 
     // TODO: loop through values and randomly ping up
 
-
-
-
-
-    /**
-
-    std::vector<double> v;
-
-
     std::random_device rand;
     std::mt19937 generate(rand());
-    std::uniform_real_distribution<double> dist(0, 100);
+    std::uniform_int_distribution<> dist(extrema.min, extrema.max);
 
-    int length = 10 * 100;
-    int guess = 0;
+    int length = data.size();
+    int guess = 0, dataPoint = 0;
     double hit = 0;
-    double miss = 0;
-
-    for (double i = 0; i < length; i = i + 0.1)
-    {
-        v.push_back(i);
-    }
-
-    std::cout << v.at(1000) << std::endl;
+    int accuracy = 100;
+    double totalGuesses = accuracy * length;
 
     for (int i = 0; i < length; i++)
     {
 
-        guess = dist(generate);
+        dataPoint = std::stoi(data.at(i).at(1));
 
-        if (guess < v.at(i))
+        for (int i = 0; i < accuracy; i++)
         {
-            hit++;
+
+            guess = dist(generate);
+
+            if (guess < dataPoint)
+            {
+                hit++;
+            }
         }
-        else
-        {
-            miss++;
-        }
+        std::cout << "index: " << i << " | point: " << dataPoint << " | hits: " << hit << std::endl;
     }
 
-    double ratio = hit / miss;
+    double ratio = hit / totalGuesses;
 
-    std::cout << "Ratio: " << ratio << " | Hits: " << hit << " | Misses: " << miss << 
-    
-    std::endl;
+    std::cout << "======== REPORT =========" << std::endl;
 
+    std::cout << "length: " << length << " | accuracy: " << accuracy << " | Total guesses: " << totalGuesses << std::endl;
+
+    std::cout << "Ratio: " << ratio << " | Hits: " << hit <<
+
+        std::endl;
     
-    */
+    std::cout << "AREA: " << ratio * (extrema.max * length) << std::endl;
 
     return 0;
 }
